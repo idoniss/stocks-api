e4d90d15aa4167606032949967117c838f47f87f
Ido Nissim <idon536@gmail.com>
2026-04-19 14:53:46 +0300

    Remove calculator endpoints

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException
 import requests
 
 app = FastAPI()
@@ -33,31 +33,3 @@ def get_stock_price(symbol: str):
         "change": quote["09. change"],
         "change_percent": quote["10. change percent"],
     }
-
-
-def compute(left: float, operator: str, right: float) -> float:
-    if operator == "+":
-        return left + right
-    elif operator == "-":
-        return left - right
-    elif operator == "*":
-        return left * right
-    elif operator == "/":
-        if right == 0:
-            raise HTTPException(status_code=400, detail="Division by zero")
-        return left / right
-    else:
-        raise HTTPException(
-            status_code=400,
-            detail=f"Unknown operator '{operator}'. Use +, -, *, /",
-        )
-
-
-@app.get("/calculate")
-def calculate(
-    left: float = Query(description="Left operand"),
-    operator: str = Query(description="Operator: +, -, *, /"),
-    right: float = Query(description="Right operand"),
-):
-    result = compute(left, operator, right)
-    return {"result": result}
